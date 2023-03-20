2529fd880b69370d5c7cb88b867c0a2a579a3216
Daniel Ronald <96017424+D-Ronald@users.noreply.github.com>
2023-03-19 21:49:33 -0300

    Update Capitulo.java

--- História/src/Capítulo/Capitulo.java
+++ História/src/Capítulo/Capitulo.java
@@ -8,52 +8,118 @@ public class Capitulo {
     String nome;
     String Texto;
     String[] escolhas;
+    public Capitulo proximoEscolha1;
+    public Capitulo proximoEscolha2;
     Personagem personagem;
-    int alteracaoEnergia;
+    public int alteracaoEnergia1;
+    public int alteracaoEnergia2;
     public int escolhido;
-    Scanner entrada= new Scanner(System.in);
+    Scanner entrada = new Scanner(System.in);
 
-    public Capitulo(String nome, String texto,String[] escolhas, Personagem personagem,
-            int alteracaoEnergia, Scanner entrada) {
+    public Capitulo(String nome, String texto, String[] escolhas, Personagem personagem,
+            int alteracaoEnergia1, int alteracaoEnergia2, Scanner entrada) {
         this.nome = nome;
-        Texto = texto;
+        this.Texto = texto;
         this.escolhas = escolhas;
         this.personagem = personagem;
-        this.alteracaoEnergia = alteracaoEnergia;
+        this.alteracaoEnergia1 = alteracaoEnergia1;
+        this.alteracaoEnergia2 = alteracaoEnergia2;
         this.entrada = entrada;
     }
 
-    public void mostrar() {
-        System.out.println(Texto);
-        if (escolhas.length == 0) {
-            System.out.println("Não há escolhas");
-        }
-        else{
-            System.out.println("Escolha uma opção: ");
-        for (int i = 0; i < escolhas.length; i++) {
-            System.out.println(i+1 + " - " + escolhas[i]);
-        }
-        int escolha= escolha();
-        if(escolha == -1){
-            System.out.println("Opção inválida");
-        }
+    public Capitulo(String nome, String texto, Personagem personagem) {
+        this.nome = nome;
+        this.Texto = texto;
+        this.personagem = personagem;
+    }
+
+    public Capitulo(String nome, String texto, Personagem personagem, int alteracaoEnergia) {
+        this.nome = nome;
+        this.Texto = texto;
+        this.personagem = personagem;
+    }
+
+    public void comEscolhas() {
+        mostrarEscolhas();
+        int escolha = escolha();
+        escolhaInvalida(escolha);
         if (escolha == 0) {
-            personagem.energia -= alteracaoEnergia;
+            personagem.energia -= alteracaoEnergia1;
+
+            proximoEscolha1.mostrar();
+            if (semEnergia()) {
+                morreu();
+            }
+        } else if (escolha == 1) {
+            personagem.energia -= alteracaoEnergia2;
+
+            proximoEscolha2.mostrar();
+            if (semEnergia()) {
+                morreu();
+            }
         }
-        System.out.println("Energia: " + personagem.energia);
-    }   
     }
 
     public int escolha() {
         String escolha = entrada.nextLine();
-                for (int i = 0; i < escolhas.length; i++) {
+        for (int i = 0; i < escolhas.length; i++) {
             if (escolhas[i].equals(escolha)) {
                 this.escolhido = i;
                 return i;
             }
         }
         return -1;
-        
+
     }
-    
+
+    public void mostrarEscolhas() {
+        System.out.println("Escolhas:");
+        for (int i = 0; i < this.escolhas.length; i++) {
+            System.out.println(this.escolhas[i]);
+        }
+        System.out.print("opção: ");
+    }
+
+    public void escolhaInvalida(int escolha) {
+        if (escolha == -1) {
+            System.out.println("Escolha inválida");
+            mostrar();
+            escolha = escolha();
+            escolhaInvalida(escolha);
+        }
+    }
+
+    public void mostrarEnegia() {
+        System.out.println("Energia: " + personagem.energia);
+    }
+
+    public boolean semEnergia() {
+        if (personagem.energia <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void morreu() {
+        System.out.println(personagem.nome + "ficou sem energia");
+        System.out.println("Fim");
+        System.exit(0);
+    }
+
+    public void mostrar() {
+        System.out.println("-----------" + this.nome + "-----------\n" + this.Texto);
+
+        if (this.escolhas == null || this.escolhas.length == 0) {
+            personagem.energia -= 100;
+            if (semEnergia()) {
+                morreu();
+            }
+        } else {
+            mostrarEnegia();
+            comEscolhas();
+
+        }
+
+    }
+
 }
